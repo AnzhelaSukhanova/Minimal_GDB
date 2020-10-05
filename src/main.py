dc41f62d647c0f5e03bec894f61c725bda54caad
AnzhelaSukhanova <bidelya@gmail.com>
2020-10-05 22:25:58 +0300

    assignment5 in developing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import argparse
-from pyformlang.cfg import Terminal, Variable
+from pyformlang.cfg import Terminal
 from pygraphblas import *
 from classes import Graph
 import check_time
@@ -48,6 +48,46 @@ def cfpq_hellings(graph, cfg):
         return reachability
 
 
+def cfpq_MxM(graph, cfg):
+    if graph.size == 0:
+        return False
+    else:
+        res = Graph()
+        res.size = graph.size
+        if cfg.generate_epsilon():
+            res.label_boolM[cfg.start_symbol] = Matrix.identity(BOOL, graph.size)
+        terminal_i = tools.indices_of_dup(list(map(grammar.body_term, cfg.productions)))
+        terminal_i.pop(None)
+        for label in graph.label_boolM:
+            terminal = Terminal(label)
+            if terminal in terminal_i:
+                for k in range(len(terminal_i[terminal])):
+                    var = list(cfg.productions)[terminal_i[terminal][k]].head
+                    if var in res.label_boolM:
+                        res.label_boolM[var] += graph.label_boolM[label]
+                    else:
+                        res.label_boolM[var] = graph.label_boolM[label]
+        res_changes = True
+        prod_var = grammar.pair_in_body(cfg.productions)
+        while res_changes:
+            res_changes = False
+            for prod in prod_var:
+                last_nvals = res.label_boolM[prod.head].nvals
+                with semiring.LOR_LAND_BOOL:
+                    res.label_boolM[prod.head] += res.label_boolM[list(prod.body)[0]] @ \
+                                                  res.label_boolM[list(prod.body)[1]]
+                if res.label_boolM[prod.head].nvals != last_nvals:
+                    res_changes = True
+        return res.label_boolM[cfg.start_symbol]
+
+
+def cfpq_tensor(graph, cfg):
+    if graph.size == 0:
+        return False
+    else:
+        return 0
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -68,6 +108,11 @@ if __name__ == '__main__':
         cfg = grammar.scan_cfg(args.files[1])
         cfg_in_crf = grammar.to_crf(cfg)
         res = cfpq_hellings(graph, cfg_in_crf)
+        if res is False:
+            print(res)
+        else:
+            print(res.nvals)
+        res = cfpq_MxM(graph, cfg_in_crf)
         if res is False:
             print(res)
         else:
