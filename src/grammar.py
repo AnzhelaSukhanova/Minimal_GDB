c633c96e508438ae1618cdcbb3be92cd80dd83bc
AnzhelaSukhanova <bidelya@gmail.com>
2020-11-01 12:32:46 +0300

    refactoring

--- src/grammar.py
+++ src/grammar.py
@@ -36,8 +36,7 @@ def to_cnf(cfg):
                 res.productions.add(Production(new_start_symbol, production.body))
             res.productions.add(production)
         return res
-    else:
-        return cfg.to_normal_form()
+    return cfg.to_normal_form()
 
 
 def to_crf(cfg):
@@ -57,32 +56,31 @@ def cyk(cfg, word):
     word_size = len(word)
     if word_size == 0:
         return cfg.generate_epsilon()
-    else:
-        var_n = len(cfg.variables)
-        matrix = [[[0 for _ in range(word_size)] for _ in range(word_size)] for _ in range(var_n)]
-        var_i = dict(zip(cfg.variables, range(var_n)))
-        sym_i = tools.indices_of_dup(word)
-        terminal_i = tools.indices_of_dup(list(map(body_term, cfg.productions)))
-        for sym in word:
-            terminal = Terminal(sym) if sym != ' ' else Epsilon()
-            if terminal in terminal_i:
-                for i in sym_i[sym]:
-                    for j in terminal_i[terminal]:
-                        matrix[var_i[list(cfg.productions)[j].head]][i][i] = 1
-        for m in range(1, word_size):
-            for i in range(word_size - m):
-                j = i + m
-                for var in range(var_n):
-                    for production in cfg.productions:
-                        for k in range(i, j):
-                            if production.head == tools.get_key(var_i, var) and \
-                                    len(production.body) == 2:
-                                matrix[var][i][j] += matrix[var_i[list(production.body)[0]]][i][k] * \
-                                                     matrix[var_i[list(production.body)[1]]][k + 1][j]
-                                if matrix[var][i][j]:
-                                    break
-                        if matrix[var][i][j]:
-                            break
+    var_n = len(cfg.variables)
+    matrix = [[[0 for _ in range(word_size)] for _ in range(word_size)] for _ in range(var_n)]
+    var_i = dict(zip(cfg.variables, range(var_n)))
+    sym_i = tools.indices_of_dup(word)
+    terminal_i = tools.indices_of_dup(list(map(body_term, cfg.productions)))
+    for sym in word:
+        terminal = Terminal(sym) if sym != ' ' else Epsilon()
+        if terminal in terminal_i:
+            for i in sym_i[sym]:
+                for j in terminal_i[terminal]:
+                    matrix[var_i[list(cfg.productions)[j].head]][i][i] = 1
+    for m in range(1, word_size):
+        for i in range(word_size - m):
+            j = i + m
+            for var in range(var_n):
+                for production in cfg.productions:
+                    for k in range(i, j):
+                        if production.head == tools.get_key(var_i, var) and \
+                                len(production.body) == 2:
+                            matrix[var][i][j] += matrix[var_i[list(production.body)[0]]][i][k] * \
+                                                 matrix[var_i[list(production.body)[1]]][k + 1][j]
+                            if matrix[var][i][j]:
+                                break
+                    if matrix[var][i][j]:
+                        break
     return bool(matrix[var_i[cfg.start_symbol]][0][word_size - 1])
 
 
--- src/main.py
+++ src/main.py
@@ -10,108 +10,105 @@ import grammar
 def cfpq_hellings(graph, cfg):
     if graph.size == 0:
         return False
-    else:
-        var_vertices = list()
-        if cfg.generate_epsilon():
-            for i in range(graph.size):
-                var_vertices.append([cfg.start_symbol, i, i])
-        terminal_i = tools.indices_of_dup(list(map(grammar.body_term, cfg.productions)))
-        terminal_i.pop(None)
-        for label in graph.label_boolM:
-            terminal = Terminal(label)
-            if terminal in terminal_i:
-                for k in range(len(terminal_i[terminal])):
-                    var = list(cfg.productions)[terminal_i[terminal][k]].head
-                    for i in range(graph.size):
-                        for j in range(graph.size):
-                            if (i, j) in graph.label_boolM[label]:
-                                var_vertices.append([var, i, j])
-        triple = var_vertices.copy()
-        prod_var = grammar.pair_in_body(cfg.productions)
-        while triple:
-            var1, v1, v2 = triple.pop()
-            for var2, u1, u2 in var_vertices:
-                if u2 == v1:
-                    for prod in prod_var:
-                        if list(prod.body) == [var2, var1] and (prod.head, u1, v2) not in var_vertices:
-                            triple.append((prod.head, u1, v2))
-                            var_vertices.append((prod.head, u1, v2))
-                elif u1 == v2:
-                    for prod in prod_var:
-                        if list(prod.body) == [var1, var2] and (prod.head, v1, u1) not in var_vertices:
-                            triple.append((prod.head, v1, u1))
-                            var_vertices.append((prod.head, v1, u1))
-        reachability = Matrix.sparse(BOOL, graph.size, graph.size)
-        for var, i, j in var_vertices:
-            if var == cfg.start_symbol:
-                reachability[i, j] = 1
-        return reachability
+    var_vertices = list()
+    if cfg.generate_epsilon():
+        for i in range(graph.size):
+            var_vertices.append([cfg.start_symbol, i, i])
+    terminal_i = tools.indices_of_dup(list(map(grammar.body_term, cfg.productions)))
+    terminal_i.pop(None)
+    for label in graph.label_boolM:
+        terminal = Terminal(label)
+        if terminal in terminal_i:
+            for k in range(len(terminal_i[terminal])):
+                var = list(cfg.productions)[terminal_i[terminal][k]].head
+                for i in range(graph.size):
+                    for j in range(graph.size):
+                        if (i, j) in graph.label_boolM[label]:
+                            var_vertices.append([var, i, j])
+    triple = var_vertices.copy()
+    prod_var = grammar.pair_in_body(cfg.productions)
+    while triple:
+        var1, v1, v2 = triple.pop()
+        for var2, u1, u2 in var_vertices:
+            if u2 == v1:
+                for prod in prod_var:
+                    if list(prod.body) == [var2, var1] and (prod.head, u1, v2) not in var_vertices:
+                        triple.append((prod.head, u1, v2))
+                        var_vertices.append((prod.head, u1, v2))
+            elif u1 == v2:
+                for prod in prod_var:
+                    if list(prod.body) == [var1, var2] and (prod.head, v1, u1) not in var_vertices:
+                        triple.append((prod.head, v1, u1))
+                        var_vertices.append((prod.head, v1, u1))
+    reachability = Matrix.sparse(BOOL, graph.size, graph.size)
+    for var, i, j in var_vertices:
+        if var == cfg.start_symbol:
+            reachability[i, j] = 1
+    return reachability
 
 
 def cfpq_MxM(graph, cfg):
     if graph.size == 0:
         return False
-    else:
-        res = Graph()
-        res.size = graph.size
-        res.label_boolM[cfg.start_symbol] = Matrix.sparse(BOOL, res.size, res.size)
-        if cfg.generate_epsilon():
-            res.label_boolM[cfg.start_symbol] += Matrix.identity(BOOL, graph.size)
-        terminal_i = tools.indices_of_dup(list(map(grammar.body_term, cfg.productions)))
-        terminal_i.pop(None)
-        for label in graph.label_boolM:
-            terminal = Terminal(label)
-            if terminal in terminal_i:
-                for k in range(len(terminal_i[terminal])):
-                    var = list(cfg.productions)[terminal_i[terminal][k]].head
-                    if var in res.label_boolM:
-                        res.label_boolM[var] += graph.label_boolM[label]
-                    else:
-                        res.label_boolM[var] = graph.label_boolM[label]
-        prod_var = grammar.pair_in_body(cfg.productions)
-        res_changes = True
-        while res_changes:
-            res_changes = False
-            for prod in prod_var:
-                if (prod.head in res.label_boolM) and \
-                        (list(prod.body)[0] in res.label_boolM) and \
-                        (list(prod.body)[1] in res.label_boolM):
-                    last_nvals = res.label_boolM[prod.head].nvals
-                    with semiring.LOR_LAND_BOOL:
-                        res.label_boolM[prod.head] += res.label_boolM[list(prod.body)[0]] @ \
-                                                      res.label_boolM[list(prod.body)[1]]
-                    if res.label_boolM[prod.head].nvals != last_nvals:
-                        res_changes = True
-        return res.label_boolM[cfg.start_symbol]
+    res = Graph()
+    res.size = graph.size
+    res.label_boolM[cfg.start_symbol] = Matrix.sparse(BOOL, res.size, res.size)
+    if cfg.generate_epsilon():
+        res.label_boolM[cfg.start_symbol] += Matrix.identity(BOOL, graph.size)
+    terminal_i = tools.indices_of_dup(list(map(grammar.body_term, cfg.productions)))
+    terminal_i.pop(None)
+    for label in graph.label_boolM:
+        terminal = Terminal(label)
+        if terminal in terminal_i:
+            for k in range(len(terminal_i[terminal])):
+                var = list(cfg.productions)[terminal_i[terminal][k]].head
+                if var in res.label_boolM:
+                    res.label_boolM[var] += graph.label_boolM[label]
+                else:
+                    res.label_boolM[var] = graph.label_boolM[label]
+    prod_var = grammar.pair_in_body(cfg.productions)
+    res_changes = True
+    while res_changes:
+        res_changes = False
+        for prod in prod_var:
+            if (prod.head in res.label_boolM) and \
+                    (list(prod.body)[0] in res.label_boolM) and \
+                    (list(prod.body)[1] in res.label_boolM):
+                last_nvals = res.label_boolM[prod.head].nvals
+                with semiring.LOR_LAND_BOOL:
+                    res.label_boolM[prod.head] += res.label_boolM[list(prod.body)[0]] @ \
+                                                  res.label_boolM[list(prod.body)[1]]
+                if res.label_boolM[prod.head].nvals != last_nvals:
+                    res_changes = True
+    return res.label_boolM[cfg.start_symbol]
 
 
 def cfpq_tensor(graph, cfg, rec_auto, heads):
     if graph.size == 0:
         return False
-    else:
-        res = graph.copy()
-        res.label_boolM[cfg.start_symbol.value] = Matrix.sparse(BOOL, res.size, res.size)
-        if cfg.generate_epsilon():
-            res.label_boolM[cfg.start_symbol.value] += Matrix.identity(BOOL, res.size)
-        res_changes = True
-        inter = Graph()
+    res = graph.copy()
+    res.label_boolM[cfg.start_symbol.value] = Matrix.sparse(BOOL, res.size, res.size)
+    if cfg.generate_epsilon():
+        res.label_boolM[cfg.start_symbol.value] += Matrix.identity(BOOL, res.size)
+    res_changes = True
+    inter = Graph()
+    tCl = inter.intersection(res, rec_auto).transitive_closure_squaring()
+    n = inter.size
+    while res_changes:
+        last_nvals = tCl.nvals
+        for i in range(n):
+            for j in range(n):
+                if (i, j) in tCl:
+                    s = i % rec_auto.size
+                    f = j % rec_auto.size
+                    if (s in rec_auto.start_states) and (f in rec_auto.final_states):
+                        x = i // rec_auto.size
+                        y = j // rec_auto.size
+                        res.set(heads[s, f], x, y)
         tCl = inter.intersection(res, rec_auto).transitive_closure_squaring()
-        n = inter.size
-        while res_changes:
-            last_nvals = tCl.nvals
-            for i in range(n):
-                for j in range(n):
-                    if (i, j) in tCl:
-                        s = i % rec_auto.size
-                        f = j % rec_auto.size
-                        if (s in rec_auto.start_states) and (f in rec_auto.final_states):
-                            x = i // rec_auto.size
-                            y = j // rec_auto.size
-                            res.set(heads[s, f], x, y)
-            tCl = inter.intersection(res, rec_auto).transitive_closure_squaring()
-            if tCl.nvals == last_nvals:
-                res_changes = False
-        return res.label_boolM[cfg.start_symbol.value]
+        if tCl.nvals == last_nvals:
+            res_changes = False
+    return res.label_boolM[cfg.start_symbol.value]
 
 
 if __name__ == '__main__':
